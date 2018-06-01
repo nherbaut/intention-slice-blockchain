5b0b291e88d08f6b2e4376713d10d709ed9c3cf2
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-06-01 12:49:02 +0200

    rest publish intentions

--- gnb-blockchain/publish_intention.sh
+++ gnb-blockchain/publish_intention.sh
@@ -1,1 +1,8 @@
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "top.nextnet.gnb.PublishIntention",    "target": "top.nextnet.gnb.Intention#9555",    "timestamp": "2018-06-01T10:03:25.472Z"  }' 'http://localhost:3000/api/PublishIntention'
+ID=$RANDOM
+ID_INT=$RANDOM
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.SliceOwner\",  \"sliceOwnerId\": \"SL$ID\",  \"name\": \"SL$ID\"  }" 'http://localhost:3000/api/SliceOwner'
+echo "\n"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.Intention\",     \"public\": \"false\",     \"intentionId\": \"$ID_INT\",     \"intentionData\": \"string\",     \"owner\": \"top.nextnet.gnb.SliceOwner#$ID\"}" 'http://localhost:3000/api/Intention'
+echo "\n"
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.PublishIntention\",     \"target\": \"top.nextnet.gnb.Intention#$ID_INT\"   }" 'http://localhost:3000/api/PublishIntention'
