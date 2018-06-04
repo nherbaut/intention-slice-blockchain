9454b3282f084a7f3334830653ce4b5f1f377b4e
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-06-04 23:40:40 +0200

    ok acl

--- gnb-client/slice-owner/cli.js
+++ gnb-client/slice-owner/cli.js
@@ -1,11 +1,3 @@
-#!/usr/bin/env node
-
-'use strict';
-
-
-function handle_new_intention_event(event) {
-    console.log(event.getFullyQualifiedType())
-}
 
 var userName = process.argv[2] ? process.argv[2] : "admin"
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
@@ -14,14 +6,12 @@ this.bizNetworkConnection = new BusinessNetworkConnection();
 this.businessNetworkDefinition = null
 this.bizNetworkConnection.connect(cardname).then(value => {
     this.businessNetworkDefinition = value;
-    this.bizNetworkConnection.on('event', (evt) => {
+    this.bizNetworkConnection.getAssetRegistry("top.nextnet.gnb.Intention").then(intentionRegistry => {
+	intentionRegistry.getAll().then( intentions => { 
+		for (let i of intentions){
+ 			console.log(i.getIdentifier()+"@"+i.owner.getIdentifier());
+		}
+	})
+   })
 
-        if (evt.getFullyQualifiedType() == "top.nextnet.gnb.NewIntentionEvent") {
-
-            handle_new_intention_event(evt)
-        }
-
-    });
-
-});
-
+}).catch( err => console.log(err));
