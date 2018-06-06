ffe525bb04a4e0b5cfd967529fe9d1c8dd1a2b7a
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-06-07 01:37:16 +0200

    resource broker creation

--- gnb-blockchain/create_ressource_broker.sh
+++ gnb-blockchain/create_ressource_broker.sh
@@ -1,17 +1,11 @@
 set -x
 ID=$RANDOM
 ID_INT=$RANDOM
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.SliceOwner\",  \"sliceOwnerId\": \"$ID\",  \"name\": \"$ID\"  }" 'http://localhost:3000/api/SliceOwner'
-echo "\n"
-
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.Intention\",     \"public\": \"false\",     \"intentionId\": \"$ID_INT\",     \"intentionData\": \"string\",     \"owner\": \"top.nextnet.gnb.SliceOwner#$ID\"}" 'http://localhost:3000/api/Intention'
-
-echo "\n"
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "{     \"$class\": \"top.nextnet.gnb.PublishIntention\",     \"target\": \"top.nextnet.gnb.Intention#$ID_INT\"   }" 'http://localhost:3000/api/PublishIntention'
+composer participant add  -d "{ \"\$class\": \"top.nextnet.gnb.ResourceBroker\",  \"id\": \"$ID\" }" -c "admin@gnb" 
 
 
 
-composer identity issue -c admin@gnb -f so$ID.card -u $ID -a "resource:top.nextnet.gnb.SliceOwner#$ID"
+composer identity issue -c admin@gnb -f rb$ID.card -u $ID -a "resource:top.nextnet.gnb.ResourceBroker#$ID"
 
 
-composer card  import -f ./so$ID.card 
+composer card  import -f ./rb$ID.card 
