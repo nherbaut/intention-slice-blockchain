a907a35cd322df0452af7ca1b003d383543570a2
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-06-08 08:47:08 +0200

    dummy service creation

--- gnb-client/resource-broker/cli.js
+++ gnb-client/resource-broker/cli.js
@@ -9,6 +9,7 @@ var fs = require('fs');
 var _ = require('underscore');
 var rn = require('random-number');
 var chalk = require('chalk');
+const uuid = require('uuid/v4');
 let config = require('config').get('event-app');
 const LOG = winston.loggers.get('application');
 let cardname = config.get('cardname');
@@ -29,10 +30,40 @@ class SitechainListener {
 	async init() {
 		this.businessNetworkDefinition = await this.bizNetworkConnection.connect(cardname);
 		this.intentionRegistry = await this.bizNetworkConnection.getAssetRegistry("top.nextnet.gnb.Intention");
+		this.serviceRegistry = await this.bizNetworkConnection.getAssetRegistry("top.nextnet.gnb.Service");
+		this.factory = await this.bizNetworkConnection.getBusinessNetwork().getFactory();
 	}
 
 
+	get_random_slice() {
+		var slice = this.factory.newConcept("top.nextnet.gnb", "TransportSlice")
+		slice.src = "" + Math.floor(Math.random() * 100);
+		slice.dst = "" + Math.floor(Math.random() * 100);
+		slice.bandwidth = Math.floor(Math.random() * 100);
+		slice.latency = Math.floor(Math.random() * 20) + 5;
 
+		return slice;
+
+	}
+	generate_services(intention) {
+		var intentionData = intention.intentionData
+
+		var services = []
+		for (let i of [0, 1, 2]) {
+			var service = this.factory.newResource("top.nextnet.gnb", "Service", uuid());
+			service.slices = []
+			for (let j of [0, 1, 2, 3]) {
+				service.slices.push(this.get_random_slice());
+			}
+			service.public = true;
+			service.intention = this.factory.newRelationship("top.nextnet.gnb", "Intention", intention.getIdentifier());
+			services.push(service);
+		}
+
+		return services;
+
+
+	}
 
 
 	/** Listen for the sale transaction events
@@ -44,16 +75,22 @@ class SitechainListener {
 			if (evt.getFullyQualifiedType() == "top.nextnet.gnb.NewIntentionEvent") {
 
 
-				this.intentionRegistry.getAll().then( all => { console.log(all)});
+
 
 				this.intentionRegistry.get(evt.target.getIdentifier()).then(
-					intention => { console.log(intention.getIdentifier()); }
+					intention => {
+
+						var services = this.generate_services(intention)
+						this.serviceRegistry.addAll(services).then(
+							console.log(services.length + " services added"));
+					}
+
 				)
-
 			}
+		})
 
-		});
 	}
+
 
 
 }
